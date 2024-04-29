546859e579b3abe039ba982aefb9528d4c877276
mark-lin-pub <michele.pr.corsi+github@gmail.com>
2024-04-30 00:45:03 +0200

    secrets and variable management

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 import express from 'express';
+import * as fs from 'fs';
+
 const app = express();
 const port = process.env.PORT || undefined;
 const env = process.env.NODE_ENV || 'none';
+const sample = process.env.SAMPLE || 'none';
+const secret = process.env.DB_PASSWORD || 'none';
+
+
 
 app.get('/', (req, res) => {
-  res.send(`Hello ${env}!`);
+  res.send(`Hello ${env} - ${sample}!`);
 });
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port} env: ${env}`);
+  const words = fs.readFileSync(secret, 'utf-8');
+  return console.log(`Express is listening at http://localhost:${port} env: ${env} sample: ${sample} secret: ${words}`);
 });
+
+
+
+
