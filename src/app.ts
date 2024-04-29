1f9d71da998516cbeec636b83c95cbb1ef682399
mark-lin-pub <michele.pr.corsi+github@gmail.com>
2024-04-29 23:27:45 +0200

    dockerfile for non development environment

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || undefined;
+const env = process.env.NODE_ENV || 'none';
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`Hello ${env}!`);
 });
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+  return console.log(`Express is listening at http://localhost:${port} env: ${env}`);
 });
